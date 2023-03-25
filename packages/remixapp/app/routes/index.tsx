556b59cced1b5d03e08b1b2c198943d71771a182
DJ Bauch <dj.bauch@gmail.com>
2023-03-25 01:10:31 -0500

    Added scope to packages

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,14 +1,12 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import { Header } from '@djbauch/header'
+import { Footer } from '@djbauch/footer'
 
 export default function Index() {
-    return (
-        <>
-            <Header/>
-            <div>
-                Content!
-            </div>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div>Content!</div>
+      <Footer />
+    </>
+  )
 }
