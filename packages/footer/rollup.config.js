e086c030ad2dc1fb246477a2265e900572d57d9b
DJ Bauch <dj.bauch@gmail.com>
2023-03-25 23:27:59 -0500

    Making devDependencies

--- packages/footer/rollup.config.js
+++ packages/footer/rollup.config.js
@@ -1,17 +1,16 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript2'
 
+const packageJson = require('./package.json')
 export default {
-    input: ["src/index.tsx"],
-    output: [
-        {
-            dir: "dist",
-            entryFileNames: "[name].js",
-            format: "cjs",
-            exports: "named"
-        }
-    ],
-    plugins: [
-        typescript(),
-    ],
-    external: ["react"]
-};
+  input: ['src/index.tsx'],
+  output: [
+    {
+      dir: 'dist',
+      entryFileNames: '[name].js',
+      format: 'cjs',
+      exports: 'named'
+    }
+  ],
+  plugins: [typescript()],
+  external: ['react']
+}
