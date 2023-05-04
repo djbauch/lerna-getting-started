d95c4d0ea2ab71ce52ba7f6018b23f791d59c0bd
DJ Bauch <dj.bauch@gmail.com>
2023-05-04 15:04:15 -0500

    Changed jest config

--- packages/footer/jest.config.ts
+++ packages/footer/jest.config.ts
@@ -1,7 +1,10 @@
-module.exports = {
+import type { Config } from 'jest'
+const config: Config = {
     testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
+    },
+    testEnvironment: "jsdom"
     }
-};
+export default config
