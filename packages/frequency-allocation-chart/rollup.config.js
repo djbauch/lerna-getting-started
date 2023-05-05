161a36a5c9de701e311e11e12bd73db709ee4818
Danny_Joe Bauch <danny_joe.bauch@URN.net>
2023-05-05 14:29:49 -0500

    Working on exporting types and css from fac

--- packages/frequency-allocation-chart/rollup.config.js
+++ packages/frequency-allocation-chart/rollup.config.js
@@ -1,35 +1,44 @@
-import typescript from 'rollup-plugin-typescript2'
+import typescript from '@rollup/plugin-typescript'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 import scss from 'rollup-plugin-scss'
+import dts from 'rollup-plugin-dts'
 import packageJson from './package.json' assert { type: 'json' }
 
-export default {
-  input: ['src/index.tsx'],
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDirective: true }),
-    scss( { fileName: 'FrequencyBand.css'}),
-    postcss({
-      extensions: ['.css']
-    })
-  ],
-  external: ['react']
-}
+const config = [
+  {
+    input: ['src/index.ts'],
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+       // plugins: [scss({ fileName: './src/FrequencyBand.css' })]
+      }
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.lib.json' }),
+      postcss({
+        extensions: ['.css']
+      })
+    ],
+    external: ['react']
+  },
+  // {
+  //   input: ['./dist/out-tsc/src/index.d.ts'],
+  //   output: ['./dist/index.d.ts'],
+  //   plugins: [dts()]
+  // }
+]
+export default config
